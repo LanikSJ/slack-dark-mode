d27d9587ac68d01269e84aa917cb428ad7b06904
LanikSJ <laniksj@gmail.com>
2019-03-24 15:21:41 -0700

    Update Shell Script to Execute on Mac Only

--- slack-dark-mode.sh
+++ slack-dark-mode.sh
@@ -3,15 +3,19 @@
 # Homebaked Slack Dark Mode. After executing this script, hit refresh (⌘ + R) or restart Slack for changes to take effect.
 # Adopted from https://gist.github.com/a7madgamal/c2ce04dde8520f426005e5ed28da8608
 
-SLACK_RESOURCES_DIR="/Applications/Slack.app/Contents/Resources"
-SLACK_FILEPATH="$SLACK_RESOURCES_DIR/app.asar.unpacked/src/static/ssb-interop.js"
-THEME_FILEPATH="$SLACK_RESOURCES_DIR/dark-theme.css"
+if [[ uname == Darwin ]]; then
 
-#curl -sSL -o "$THEME_FILEPATH" "https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css"
-cp -af dark-theme.css "$THEME_FILEPATH"
+  SLACK_RESOURCES_DIR="/Applications/Slack.app/Contents/Resources"
+  SLACK_FILEPATH="$SLACK_RESOURCES_DIR/app.asar.unpacked/src/static/ssb-interop.js"
+  THEME_FILEPATH="$SLACK_RESOURCES_DIR/dark-theme.css"
 
-echo "Modifying Slack... "
+  #curl -sSL -o "$THEME_FILEPATH" "https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css"
+  cp -af dark-theme.css "$THEME_FILEPATH"
 
-cat event-listener.js >> "$SLACK_FILEPATH"
+  echo "Modifying Slack... "
 
-echo "Done! After executing this script, hit refresh (⌘ + R) or restart Slack for changes to take effect."
+  cat event-listener.js >> "$SLACK_FILEPATH"
+
+  echo "Done! After executing this script, hit refresh (⌘ + R) or restart Slack for changes to take effect."
+
+fi
