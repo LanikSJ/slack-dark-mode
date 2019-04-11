3a0e50c0f97b3cdb227451a55c497414972372e5
Lanik <LanikSJ@users.noreply.github.com>
2019-04-10 19:49:09 -0700

    Update slack-dark-mode.sh

--- slack-dark-mode.sh
+++ slack-dark-mode.sh
@@ -4,20 +4,21 @@
 # Adopted from https://gist.github.com/a7madgamal/c2ce04dde8520f426005e5ed28da8608
 
 SLACK_RESOURCES_DIR="/Applications/Slack.app/Contents/Resources"
+SLACK_EVENT_LISTENER="event-listener.js"
 SLACK_FILEPATH="$SLACK_RESOURCES_DIR/app.asar.unpacked/src/static/ssb-interop.js"
 THEME_FILEPATH="$SLACK_RESOURCES_DIR/dark-theme.css"
 
 #curl -sSL -o "$THEME_FILEPATH" "https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css"
 
 OWNER=$(stat -f "%Su" /Applications/Slack.app)
-if [[ $OWNER == root ]]; then 
+if [[ $OWNER == root ]]; then
+  echo "Add Dark Theme to Slack... "
   sudo cp -af dark-theme.css "$THEME_FILEPATH"
-  echo "Add Dark Theme to Slack... " 
-  cat event-listener.js | sudo tee -a "$SLACK_FILEPATH"
-else 
+  echo $SLACK_EVENT_LISTENER | sudo tee -a "$SLACK_FILEPATH"
+else
+  echo "Add Dark Theme to Slack... "
   cp -af dark-theme.css "$THEME_FILEPATH"
-  echo "Add Dark Theme to Slack... " 
-  cat event-listener.js >> "$SLACK_FILEPATH"
+  echo $SLACK_EVENT_LISTENER >> "$SLACK_FILEPATH"
 fi
 
 echo "Done! After executing this script, hit refresh (⌘ + R) or restart Slack for changes to take effect."
