93f9d482ae2b28857e540778a76000ea8dcf27a3
Hans Kristian Flaatten <hans@starefossen.com>
2019-04-11 20:07:29 +0200

    Use sudo to modify Slack installations through Mac AppStore (#45)
    * Use sudo to modify Slack installations through Mac AppStore
    
    * Update slack-dark-mode.sh
    
    * Update slack-dark-mode.sh
    
    * Update slack-dark-mode.sh
    
    * Update slack-dark-mode.sh
    
    * Update slack-dark-mode.sh

--- slack-dark-mode.sh
+++ slack-dark-mode.sh
@@ -4,14 +4,14 @@
 # Adopted from https://gist.github.com/a7madgamal/c2ce04dde8520f426005e5ed28da8608
 
 SLACK_RESOURCES_DIR="/Applications/Slack.app/Contents/Resources"
+SLACK_EVENT_LISTENER="event-listener.js"
 SLACK_FILEPATH="$SLACK_RESOURCES_DIR/app.asar.unpacked/src/static/ssb-interop.js"
 THEME_FILEPATH="$SLACK_RESOURCES_DIR/dark-theme.css"
 
 #curl -sSL -o "$THEME_FILEPATH" "https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css"
-cp -af dark-theme.css "$THEME_FILEPATH"
 
-echo "Modifying Slack... "
-
-cat event-listener.js >> "$SLACK_FILEPATH"
+echo "Adding Dark Theme Code to Slack... "
+sudo cp -af dark-theme.css "$THEME_FILEPATH"
+sudo tee -a "$SLACK_FILEPATH" < $SLACK_EVENT_LISTENER
 
 echo "Done! After executing this script, hit refresh (⌘ + R) or restart Slack for changes to take effect."
