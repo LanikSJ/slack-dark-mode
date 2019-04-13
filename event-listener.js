00e4fa7b4cfd2ae1f93e46672d37434e7d30c2d2
Jordan Lucier <jordan@perch.fit>
2019-04-13 14:23:13 -0400

    Adding linux support

--- event-listener.js
+++ event-listener.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const fs = require('fs');
-  const filePath = '/Applications/Slack.app/Contents/Resources/dark-theme.css';
+  const filePath = 'SLACK_DARK_THEME_PATH';
   const tt__customCss = '.menu ul li a:not(.inline_menu_link) {color: #fff !important;}'
   fs.readFile(filePath, {
     encoding: 'utf-8'
--- slack-dark-mode.sh
+++ slack-dark-mode.sh
@@ -3,7 +3,16 @@
 # Homebaked Slack Dark Mode. After executing this script, hit refresh (⌘ + R) or restart Slack for changes to take effect.
 # Adopted from https://gist.github.com/a7madgamal/c2ce04dde8520f426005e5ed28da8608
 
-SLACK_RESOURCES_DIR="/Applications/Slack.app/Contents/Resources"
+OSX_SLACK_RESOURCES_DIR="/Applications/Slack.app/Contents/Resources"
+LINUX_SLACK_RESOURCES_DIR="/usr/lib/slack/resources"
+
+if [ -d $OSX_SLACK_RESOURCES_DIR ]
+then
+    SLACK_RESOURCES_DIR=$OSX_SLACK_RESOURCES_DIR
+else
+    SLACK_RESOURCES_DIR=$LINUX_SLACK_RESOURCES_DIR
+fi
+
 SLACK_EVENT_LISTENER="event-listener.js"
 SLACK_FILEPATH="$SLACK_RESOURCES_DIR/app.asar.unpacked/src/static/ssb-interop.js"
 THEME_FILEPATH="$SLACK_RESOURCES_DIR/dark-theme.css"
@@ -12,6 +21,7 @@ THEME_FILEPATH="$SLACK_RESOURCES_DIR/dark-theme.css"
 
 echo "Adding Dark Theme Code to Slack... "
 sudo cp -af dark-theme.css "$THEME_FILEPATH"
+sed -i -e s@SLACK_DARK_THEME_PATH@$THEME_FILEPATH@g $SLACK_EVENT_LISTENER
 sudo tee -a "$SLACK_FILEPATH" < $SLACK_EVENT_LISTENER
 
 echo "Done! After executing this script, hit refresh (⌘ + R) or restart Slack for changes to take effect."
