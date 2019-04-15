47a7712583f5d2dfc8073a3ba65697f71bf9b5f4
LanikSJ <laniksj@gmail.com>
2019-04-15 10:54:30 -0700

    Update slack-dark-mode.sh

--- slack-dark-mode.sh
+++ slack-dark-mode.sh
@@ -6,12 +6,8 @@
 OSX_SLACK_RESOURCES_DIR="/Applications/Slack.app/Contents/Resources"
 LINUX_SLACK_RESOURCES_DIR="/usr/lib/slack/resources"
 
-if [ -d $OSX_SLACK_RESOURCES_DIR ]
-then
-    SLACK_RESOURCES_DIR=$OSX_SLACK_RESOURCES_DIR
-else
-    SLACK_RESOURCES_DIR=$LINUX_SLACK_RESOURCES_DIR
-fi
+if [[ -d $OSX_SLACK_RESOURCES_DIR ]]; then SLACK_RESOURCES_DIR=$OSX_SLACK_RESOURCES_DIR; fi
+if [[ -d $LINUX_SLACK_RESOURCES_DIR ]]; then SLACK_RESOURCES_DIR=$LINUX_SLACK_RESOURCES_DIR; fi
 
 SLACK_EVENT_LISTENER="event-listener.js"
 SLACK_FILEPATH="$SLACK_RESOURCES_DIR/app.asar.unpacked/src/static/ssb-interop.js"
@@ -21,7 +17,7 @@ THEME_FILEPATH="$SLACK_RESOURCES_DIR/dark-theme.css"
 
 echo "Adding Dark Theme Code to Slack... "
 sudo cp -af dark-theme.css "$THEME_FILEPATH"
-sed -i -e s@SLACK_DARK_THEME_PATH@$THEME_FILEPATH@g $SLACK_EVENT_LISTENER
+sudo sed -i -e s@SLACK_DARK_THEME_PATH@$THEME_FILEPATH@g $SLACK_EVENT_LISTENER
 sudo tee -a "$SLACK_FILEPATH" < $SLACK_EVENT_LISTENER
 
 echo "Done! After executing this script, hit refresh (⌘ + R) or restart Slack for changes to take effect."
