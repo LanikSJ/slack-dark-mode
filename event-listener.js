5c8b0d3df59dfd98b7e89e4cd9e77e8451631a7d
Lanik <LanikSJ@users.noreply.github.com>
2019-04-15 08:44:25 -0700

    Update event-listener.js

--- event-listener.js
+++ event-listener.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const fs = require('fs');
-  const filePath = 'SLACK_DARK_THEME_PATH';
+  const filePath = "SLACK_DARK_THEME_PATH";
   const tt__customCss = '.menu ul li a:not(.inline_menu_link) {color: #fff !important;}'
   fs.readFile(filePath, {
     encoding: 'utf-8'
