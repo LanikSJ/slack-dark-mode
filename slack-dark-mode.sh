60471cbf8e948ba09b972d06a602776e8b23b472
LanikSJ <laniksj@gmail.com>
2019-04-21 10:40:47 -0700

    Update slack-dark-mode.sh

--- slack-dark-mode.sh
+++ slack-dark-mode.sh
@@ -16,9 +16,10 @@ THEME_FILEPATH="$SLACK_RESOURCES_DIR/dark-theme.css"
 #curl -sSL -o "$THEME_FILEPATH" "https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css"
 
 echo && echo "Adding Dark Theme Code to Slack... "
+echo "This script requires sudo privileges."
 echo "You'll need to provide your password."
 sudo cp -af dark-theme.css "$THEME_FILEPATH"
 sudo tee -a "$SLACK_FILEPATH" < $SLACK_EVENT_LISTENER
-sed -i -e s@SLACK_DARK_THEME_PATH@$THEME_FILEPATH@g $SLACK_FILEPATH
+sudo sed -i -e s@SLACK_DARK_THEME_PATH@$THEME_FILEPATH@g $SLACK_FILEPATH
 
 echo && echo "Done! After executing this script, hit refresh (⌘ + R) or restart Slack for changes to take effect."
