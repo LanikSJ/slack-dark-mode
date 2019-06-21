2dc8346d43efcd8b44a3da489d8a979905255185
Matteo Gaggiano <developer@marchrius.org>
2019-06-21 20:29:23 +0200

    Added option to update only without re-insert the js script (#68)

--- slack-dark-mode.sh
+++ slack-dark-mode.sh
@@ -5,9 +5,11 @@
 
 OSX_SLACK_RESOURCES_DIR="/Applications/Slack.app/Contents/Resources"
 LINUX_SLACK_RESOURCES_DIR="/usr/lib/slack/resources"
+UPDATE_ONLY="false"
 
 if [[ -d $OSX_SLACK_RESOURCES_DIR ]]; then SLACK_RESOURCES_DIR=$OSX_SLACK_RESOURCES_DIR; fi
 if [[ -d $LINUX_SLACK_RESOURCES_DIR ]]; then SLACK_RESOURCES_DIR=$LINUX_SLACK_RESOURCES_DIR; fi
+if [[ "$1" == "-u" ]]; then UPDATE_ONLY="true"; fi
 
 SLACK_EVENT_LISTENER="event-listener.js"
 SLACK_FILEPATH="$SLACK_RESOURCES_DIR/app.asar.unpacked/src/static/ssb-interop.js"
@@ -15,11 +17,23 @@ THEME_FILEPATH="$SLACK_RESOURCES_DIR/dark-theme.css"
 
 #curl -sSL -o "$THEME_FILEPATH" "https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css"
 
-echo && echo "Adding Dark Theme Code to Slack... "
+if [[ "$UPDATE_ONLY" == "true" ]]; then
+  echo && echo "Updating Dark Theme Code to Slack... "
+else
+  echo && echo "Adding Dark Theme Code to Slack... "
+fi
+
 echo "This script requires sudo privileges."
 echo "You'll need to provide your password."
 sudo cp -af dark-theme.css "$THEME_FILEPATH"
-sudo tee -a "$SLACK_FILEPATH" < $SLACK_EVENT_LISTENER
+
+if [[ "$UPDATE_ONLY" == "true" ]]; then
+  echo && echo "Updating Dark Theme Code to Slack... "
+else
+  echo && echo "Adding Dark Theme Code to Slack... "
+  sudo tee -a "$SLACK_FILEPATH" < $SLACK_EVENT_LISTENER
+fi
+
 sudo sed -i -e s@SLACK_DARK_THEME_PATH@$THEME_FILEPATH@g $SLACK_FILEPATH
 
 echo && echo "Done! After executing this script, hit refresh (⌘ + R) or restart Slack for changes to take effect."
