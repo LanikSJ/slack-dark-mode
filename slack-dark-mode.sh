0f0e276cef8f3ecb2d6421e9f7723abe69c5b506
Theral Mackey <tmackey@evernote.com>
2019-07-23 16:46:42 -0700

    Fix sed command (-i takes an arg), pre-expand ~ to fix broken edits (#95)

--- slack-dark-mode.sh
+++ slack-dark-mode.sh
@@ -3,8 +3,9 @@
 # Homebaked Slack Dark Mode. After executing this script restart Slack for changes to take effect.
 # Adopted from https://gist.github.com/a7madgamal/c2ce04dde8520f426005e5ed28da8608
 
-SLACK_DIRECT_LOCAL_SETTINGS="~/Library/Application\ Support/Slack/local-settings.json"
-SLACK_STORE_LOCAL_SETTINGS="~/Library/Containers/com.tinyspeck.slackmacgap/Data/Library/Application\ Support/Slack/local-settings.json"
+MYHOME=$(ls -d ~)
+SLACK_DIRECT_LOCAL_SETTINGS="Library/Application Support/Slack/local-settings.json"
+SLACK_STORE_LOCAL_SETTINGS="Library/Containers/com.tinyspeck.slackmacgap/Data/Library/Application\ Support/Slack/local-settings.json"
 OSX_SLACK_RESOURCES_DIR="/Applications/Slack.app/Contents/Resources"
 LINUX_SLACK_RESOURCES_DIR="/usr/lib/slack/resources"
 UPDATE_ONLY="false"
@@ -13,8 +14,8 @@ echo && echo "This script requires sudo privileges." && echo "You'll need to pro
 
 type npx
 if [[ "$?" != "0" ]]; then echo "Please install Node for your OS.  macOS users will also need to install Homebrew from https://brew.sh"; fi
-if [[ -f $SLACK_DIRECT_LOCAL_SETTINGS ]]; then sed -i 's/"bootSonic":"once"/"bootSonic":"never"/g' $SLACK_DIRECT_LOCAL_SETTINGS; fi
-if [[ -f $SLACK_STORE_LOCAL_SETTINGS ]]; then sudo sed -i 's/"bootSonic":"once"/"bootSonic":"never"/g' $SLACK_STORE_LOCAL_SETTINGS; fi
+if [[ -f "$MYHOME/$SLACK_DIRECT_LOCAL_SETTINGS" ]]; then sed -i tmp 's/"bootSonic":"once"/"bootSonic":"never"/g' "$MYHOME/$SLACK_DIRECT_LOCAL_SETTINGS"; fi
+if [[ -f "$MYHOME/$SLACK_STORE_LOCAL_SETTINGS" ]]; then sudo sed -i tmp 's/"bootSonic":"once"/"bootSonic":"never"/g' "$MYHOME/$SLACK_STORE_LOCAL_SETTINGS"; fi
 
 if [[ -d $OSX_SLACK_RESOURCES_DIR ]]; then SLACK_RESOURCES_DIR=$OSX_SLACK_RESOURCES_DIR; fi
 if [[ -d $LINUX_SLACK_RESOURCES_DIR ]]; then SLACK_RESOURCES_DIR=$LINUX_SLACK_RESOURCES_DIR; fi
