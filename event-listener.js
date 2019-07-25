ac473d481b006618917ca427400222eb50ded07c
Ben Lipson <bjlipson@spscommerce.com>
2019-07-25 15:27:23 -0500

    Update event-listener.js

--- event-listener.js
+++ event-listener.js
@@ -2,16 +2,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   const fs = require('fs');
   const filePath = "SLACK_DARK_THEME_PATH";
-  const tt__customCss = '.menu ul li a:not(.inline_menu_link) {color: #fff !important;}'
   fs.readFile(filePath, {
     encoding: 'utf-8'
   }, function(err, css) {
-    if (!err) {
-      const head = document.getElementsByTagName('head')[0];
-      const style = document.createElement('style');
-      style.innerHTML = css + tt__customCss;
-
-      head.appendChild(style);
+      if (!err) {
+          var styleEl = document.createElement("style");
+          styleEl.innerHTML = css;
+          document.querySelector("head").append(styleEl);
     }
   });
 });
