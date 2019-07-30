570b854a76d6ae18826ebd0267552c63d0bc579e
LanikSJ <laniksj@gmail.com>
2019-07-30 16:15:45 -0700

    Update Coverage

--- slack-dark-mode.sh
+++ slack-dark-mode.sh
@@ -12,21 +12,15 @@ UPDATE_ONLY="false"
 echo && echo "This script requires sudo privileges." && echo "You'll need to provide your password."
 
 type npx
-if [[ "$?" != "0" ]]; then
-  echo "Please install Node for your OS.  macOS users will also need to install Homebrew from https://brew.sh"
-fi
+if [[ "$?" != "0" ]]; then echo "Please install Node for your OS.  macOS users will also need to install Homebrew from https://brew.sh"; fi
 
 if [[ -d $OSX_SLACK_RESOURCES_DIR ]]; then
   SLACK_RESOURCES_DIR=$OSX_SLACK_RESOURCES_DIR
 fi
 
-if [[ -d $LINUX_SLACK_RESOURCES_DIR ]]; then
-  SLACK_RESOURCES_DIR=$LINUX_SLACK_RESOURCES_DIR
-fi
+if [[ -d $LINUX_SLACK_RESOURCES_DIR ]]; then SLACK_RESOURCES_DIR=$LINUX_SLACK_RESOURCES_DIR; fi
 
-if [[ "$1" == "-u" ]]; then
-  UPDATE_ONLY="true"
-fi
+if [[ "$1" == "-u" ]]; then UPDATE_ONLY="true"; fi
 
 SLACK_EVENT_LISTENER="event-listener.js"
 SLACK_FILEPATH="$SLACK_RESOURCES_DIR/app.asar.unpacked/dist/ssb-interop.bundle.js"
@@ -34,30 +28,22 @@ THEME_FILEPATH="$SLACK_RESOURCES_DIR/dark-theme.css"
 
 #curl -sSL -o "$THEME_FILEPATH" "https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css"
 
-if [[ "$UPDATE_ONLY" == "true" ]]; then
-  echo && echo "Updating Dark Theme Code for Slack... "
-fi
+if [[ "$UPDATE_ONLY" == "true" ]]; then echo && echo "Updating Dark Theme Code for Slack... "; fi
 
 if [[ "$UPDATE_ONLY" == "false" ]]; then
   echo && echo "Adding Dark Theme Code to Slack... "
 fi
 
-if [[ -z $HOME ]]; then
-  HOME=$(ls -d ~)
-fi
+if [[ -z $HOME ]]; then HOME=$(ls -d ~); fi
 
 # Copy CSS to Slack Folder
 sudo cp -af dark-theme.css "$THEME_FILEPATH"
 
 if [[ "$UPDATE_ONLY" == "false" ]]; then
     # Modify Local Settings
-    if [[ -f "$HOME/$SLACK_DIRECT_LOCAL_SETTINGS" ]]; then
-      sed -i 's/"bootSonic":"once"/"bootSonic":"never"/g' "$HOME/$SLACK_DIRECT_LOCAL_SETTINGS"
-    fi
+    if [[ -f "$HOME/$SLACK_DIRECT_LOCAL_SETTINGS" ]]; then sed -i 's/"bootSonic":"once"/"bootSonic":"never"/g' "$HOME/$SLACK_DIRECT_LOCAL_SETTINGS"; fi
 
-    if [[ -f "$HOME/$SLACK_STORE_LOCAL_SETTINGS" ]]; then
-      sudo sed -i 's/"bootSonic":"once"/"bootSonic":"never"/g' "$HOME/$SLACK_STORE_LOCAL_SETTINGS"
-    fi
+    if [[ -f "$HOME/$SLACK_STORE_LOCAL_SETTINGS" ]]; then sudo sed -i 's/"bootSonic":"once"/"bootSonic":"never"/g' "$HOME/$SLACK_STORE_LOCAL_SETTINGS"; fi
 
     # Unpack Asar Archive for Slack
     sudo npx asar extract $SLACK_RESOURCES_DIR/app.asar $SLACK_RESOURCES_DIR/app.asar.unpacked
