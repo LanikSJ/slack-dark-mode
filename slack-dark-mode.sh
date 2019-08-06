2c3606d4c5b1d3d6cb677096c3553fe22370d4b7
Lanik <LanikSJ@users.noreply.github.com>
2019-08-05 19:03:34 -0700

    Update Script and CSS Missing Styles (#119)
    * Update Script and CSS for Announcements
    
    * Lint Shell Script
    
    * Update Coverage
    
    * Update Pinned Items Color
    
    * Revert "Update Pinned Items Color"
    
    This reverts commit 59617b6a6bfe2cb609b6bbaa2c1fbe1346cbfd71.
    
    * Update Pinned Items Color
    
    * Update Another Color Code
    
    * Update Another Color Code
    
    * Update Profile Previews
    
    * Update Profile Previews
    
    * Update Direct Message Styling

--- slack-dark-mode.sh
+++ slack-dark-mode.sh
@@ -3,7 +3,6 @@
 # Homebaked Slack Dark Mode. After executing this script restart Slack for changes to take effect.
 # Adopted from https://gist.github.com/a7madgamal/c2ce04dde8520f426005e5ed28da8608
 
-MYHOME=$(ls -d ~)
 SLACK_DIRECT_LOCAL_SETTINGS="Library/Application Support/Slack/local-settings.json"
 SLACK_STORE_LOCAL_SETTINGS="Library/Containers/com.tinyspeck.slackmacgap/Data/Library/Application\ Support/Slack/local-settings.json"
 OSX_SLACK_RESOURCES_DIR="/Applications/Slack.app/Contents/Resources"
@@ -14,11 +13,13 @@ echo && echo "This script requires sudo privileges." && echo "You'll need to pro
 
 type npx
 if [[ "$?" != "0" ]]; then echo "Please install Node for your OS.  macOS users will also need to install Homebrew from https://brew.sh"; fi
-if [[ -f "$MYHOME/$SLACK_DIRECT_LOCAL_SETTINGS" ]]; then sed -i tmp 's/"bootSonic":"once"/"bootSonic":"never"/g' "$MYHOME/$SLACK_DIRECT_LOCAL_SETTINGS"; fi
-if [[ -f "$MYHOME/$SLACK_STORE_LOCAL_SETTINGS" ]]; then sudo sed -i tmp 's/"bootSonic":"once"/"bootSonic":"never"/g' "$MYHOME/$SLACK_STORE_LOCAL_SETTINGS"; fi
 
-if [[ -d $OSX_SLACK_RESOURCES_DIR ]]; then SLACK_RESOURCES_DIR=$OSX_SLACK_RESOURCES_DIR; fi
+if [[ -d $OSX_SLACK_RESOURCES_DIR ]]; then
+  SLACK_RESOURCES_DIR=$OSX_SLACK_RESOURCES_DIR
+fi
+
 if [[ -d $LINUX_SLACK_RESOURCES_DIR ]]; then SLACK_RESOURCES_DIR=$LINUX_SLACK_RESOURCES_DIR; fi
+
 if [[ "$1" == "-u" ]]; then UPDATE_ONLY="true"; fi
 
 SLACK_EVENT_LISTENER="event-listener.js"
@@ -28,23 +29,33 @@ THEME_FILEPATH="$SLACK_RESOURCES_DIR/dark-theme.css"
 #curl -sSL -o "$THEME_FILEPATH" "https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css"
 
 if [[ "$UPDATE_ONLY" == "true" ]]; then echo && echo "Updating Dark Theme Code for Slack... "; fi
-if [[ "$UPDATE_ONLY" == "false" ]]; then echo && echo "Adding Dark Theme Code to Slack... "; fi
+
+if [[ "$UPDATE_ONLY" == "false" ]]; then
+  echo && echo "Adding Dark Theme Code to Slack... "
+fi
+
+if [[ -z $HOME ]]; then HOME=$(ls -d ~); fi
 
 # Copy CSS to Slack Folder
 sudo cp -af dark-theme.css "$THEME_FILEPATH"
 
 if [[ "$UPDATE_ONLY" == "false" ]]; then
-  # Unpack Asar Archive for Slack
-  sudo npx asar extract $SLACK_RESOURCES_DIR/app.asar $SLACK_RESOURCES_DIR/app.asar.unpacked
+    # Modify Local Settings
+    if [[ -f "$HOME/$SLACK_DIRECT_LOCAL_SETTINGS" ]]; then sed -i 's/"bootSonic":"once"/"bootSonic":"never"/g' "$HOME/$SLACK_DIRECT_LOCAL_SETTINGS"; fi
 
-  # Add JS Code to Slack
-  sudo tee -a "$SLACK_FILEPATH" < $SLACK_EVENT_LISTENER
+    if [[ -f "$HOME/$SLACK_STORE_LOCAL_SETTINGS" ]]; then sudo sed -i 's/"bootSonic":"once"/"bootSonic":"never"/g' "$HOME/$SLACK_STORE_LOCAL_SETTINGS"; fi
 
-  # Insert the CSS File Location in JS
-  sudo sed -i -e s@SLACK_DARK_THEME_PATH@$THEME_FILEPATH@g $SLACK_FILEPATH
+    # Unpack Asar Archive for Slack
+    sudo npx asar extract $SLACK_RESOURCES_DIR/app.asar $SLACK_RESOURCES_DIR/app.asar.unpacked
 
-  # Pack the Asar Archive for Slack
-  sudo npx asar pack $SLACK_RESOURCES_DIR/app.asar.unpacked $SLACK_RESOURCES_DIR/app.asar
+    # Add JS Code to Slack
+    sudo tee -a "$SLACK_FILEPATH" < $SLACK_EVENT_LISTENER
+
+    # Insert the CSS File Location in JS
+    sudo sed -i -e s@SLACK_DARK_THEME_PATH@$THEME_FILEPATH@g $SLACK_FILEPATH
+
+    # Pack the Asar Archive for Slack
+    sudo npx asar pack $SLACK_RESOURCES_DIR/app.asar.unpacked $SLACK_RESOURCES_DIR/app.asar
 fi
 
 echo && echo "Done! After executing this script restart Slack for changes to take effect."
