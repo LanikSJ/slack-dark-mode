18db4bc349b0beb60a1f163c7a8c8cfbbe4ed07e
Leon Solis <krazyito65@gmail.com>
2019-08-09 16:22:55 -0500

    codacy "issues" with redirection.

--- snap-slack-dark-mode.sh
+++ snap-slack-dark-mode.sh
@@ -58,7 +58,7 @@ if [[ "$UPDATE_ONLY" == "false" ]]; then
     sudo npx asar extract $SLACK_RESOURCES_DIR/app.asar $ssb_js_dir/app.asar.unpacked
 
     # Add JS Code to Slack
-    sudo tee -a "$SLACK_FILEPATH" < $SLACK_EVENT_LISTENER
+    cat $SLACK_EVENT_LISTENER | sudo tee -a "$SLACK_FILEPATH"
 
     # Insert the CSS File Location in JS
     sudo sed -i -e s@SLACK_DARK_THEME_PATH@$THEME_FILEPATH@g $SLACK_FILEPATH
