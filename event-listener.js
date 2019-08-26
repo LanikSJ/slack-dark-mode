19d77016dff831617307e80a6a7ac02bf17ebb0b
Fernando Nunez <jnunez2@wayfair.com>
2019-08-26 12:54:02 -0400

    Add hotkey for dark mode toggle.

--- event-listener.js
+++ event-listener.js
@@ -1,14 +1,27 @@
-
 document.addEventListener('DOMContentLoaded', function() {
   const fs = require('fs');
   const filePath = "SLACK_DARK_THEME_PATH";
+  const IS_DARK_KEY = "isDark";
+
   fs.readFile(filePath, {
     encoding: 'utf-8'
   }, function(err, css) {
     if (!err) {
-      var styleEl = document.createElement("style");
+      const styleEl = document.createElement("style");
+      const head = document.querySelector("head");
       styleEl.innerHTML = css;
-      document.querySelector("head").append(styleEl);
+
+      if (JSON.parse(localStorage.getItem(IS_DARK_KEY))) {
+        head.append(styleEl);
+      }
+
+      document.addEventListener("keydown", event => {
+        if (event.ctrlKey && event.keyCode === 76) {
+          const isDark = JSON.parse(localStorage.getItem(IS_DARK_KEY));
+          isDark ? head.removeChild(styleEl) : head.append(styleEl);
+          localStorage.setItem(IS_DARK_KEY, !isDark);
+        }
+      });
     }
   });
 });
