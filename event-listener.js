55aa20caff12ff664f3bd9d38d61fa4220bf8dc0
Lanik <LanikSJ@users.noreply.github.com>
2019-08-26 12:11:01 -0700

    Revert "Hotkey to Toggle Dark Mode (#184)"
    This reverts commit fcde78474538c61ca9c1c75df2e2b6fe019f6f00.

--- event-listener.js
+++ event-listener.js
@@ -2,27 +2,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   const fs = require('fs');
   const filePath = "SLACK_DARK_THEME_PATH";
-  const IS_DARK_KEY = "isDark";
-
   fs.readFile(filePath, {
     encoding: 'utf-8'
   }, function(err, css) {
     if (!err) {
-      const styleEl = document.createElement("style");
-      const head = document.querySelector("head");
+      var styleEl = document.createElement("style");
       styleEl.innerHTML = css;
-
-      if (JSON.parse(localStorage.getItem(IS_DARK_KEY))) {
-        head.append(styleEl);
-      }
-
-      document.addEventListener("keydown", event => {
-        if (event.ctrlKey && event.keyCode === 76) {
-          const isDark = JSON.parse(localStorage.getItem(IS_DARK_KEY));
-          isDark ? head.removeChild(styleEl) : head.append(styleEl);
-          localStorage.setItem(IS_DARK_KEY, !isDark);
-        }
-      });
+      document.querySelector("head").append(styleEl);
     }
   });
 });
